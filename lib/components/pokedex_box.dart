e98a87b0ee2597b7750df28ffe7cef3a07184eb9
TRANCHANT Marie <marie.tranchant@maif.fr>
2024-08-30 14:58:11 +0200

    page pokedex presque ok git add .git add .

--- lib/components/pokedex_box.dart
+++ lib/components/pokedex_box.dart
@@ -9,57 +9,79 @@ class PokedexBox extends StatelessWidget {
   final String name;
   final String imageUrl;
 
-  const PokedexBox(
-      {super.key,
-      required this.idLabel,
-      required this.name,
-      required this.imageUrl});
+  const PokedexBox({
+    super.key,
+    required this.idLabel,
+    required this.name,
+    required this.imageUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.all(6.0),
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        decoration: BoxDecoration(
-            boxShadow: const [
-              BoxShadow(
-                  blurRadius: 3.0,
-                  spreadRadius: 1.0,
-                  offset: const Offset(0, 1))
-            ],
-            color: PokedexColors.grayScale[100],
-            borderRadius: BorderRadius.circular(8)),
-        child: Column(
-          //crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Flex(
-              direction: Axis.horizontal,
-              textDirection: TextDirection.rtl,
-              children: [
-                Text(
-                  idLabel,
-                  textAlign: TextAlign.end,
-                  textDirection: TextDirection.ltr,
-                  style: Theme.of(context).textTheme.labelSmall,
+      margin: const EdgeInsets.all(6.0),
+      decoration: BoxDecoration(
+        boxShadow: const [
+          BoxShadow(blurRadius: 3.0, spreadRadius: 1.0, offset: Offset(0, 1))
+        ],
+        color: PokedexColors.grayScale[100],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, right: 8.0),
+                child:
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      idLabel,
+                      textAlign: TextAlign.end,
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                  ],
                 ),
-              ],
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 60,
+              decoration: BoxDecoration(
+                color: PokedexColors.grayScale[075],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ),
+              ),
             ),
-            Expanded(
-                child: Image.network(
+          ),
+          Positioned(
+            top: 20,
+            left: 0,
+            right: 0,
+            child: Image.network(
               imageUrl,
               fit: BoxFit.scaleDown,
-              width: 72,
-              height: 72,
-            )),
-            Container(
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-              child: Text(name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: '${Theme.of(context).textTheme.bodySmall}',
-                  )),
+              width: 83,
+              height: 83,
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
